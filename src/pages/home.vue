26bf3303e2d315fb61057f8f3af7eadececea6c8
k186 <k1868548@gmail.com>
2017-03-23 11:25:11 +0800

    add icon

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="k-main-title">晴天钟优化版-k186</div>
     <div class="k-nav-container">
-      <router-link to="baidu" class="k-nav-go"><span>百度</span></router-link>
-      <router-link to="gaode" class="k-nav-go"><span>高德</span></router-link>
-      <a href="https://www.k186studio.com/tools/7Timer/google" class="k-nav-go"><span>谷歌</span></a>
+      <router-link to="baidu" class="k-nav-go"><img class="k-btn-icon" src="./img/baidu.jpg" alt=""><span>百度</span></router-link>
+      <router-link to="gaode" class="k-nav-go"><img class="k-btn-icon" src="./img/gaode.jpg" alt=""><span>高德</span></router-link>
+      <a href="https://www.k186studio.com/tools/7Timer/google" class="k-nav-go"><img class="k-btn-icon" src="./img/google.jpg" alt=""><span>谷歌</span></a>
     </div>
-    <p style="font-size: 14px;color: #c9c9c9;margin: 20px">本站已经开通https!!!!</p>
-    <p style="font-size: 14px;color: #c9c9c9;margin: 20px">晴天钟官方接口极不稳定,如果无法加载出图片请间隔一段时间后重试</p>
+    <p style="font-size: 14px;color: #2a9cd9;margin: 20px">本站已经开通https!!!!</p>
+    <p style="font-size: 14px;color: #e8984c;margin: 20px">晴天钟官方接口极不稳定,如果无法加载出图片请间隔一段时间后重试</p>
     <div style="margin: 10px;">
-      <p style="font-size: 14px;color: #c9c9c9;">使用说明</p>
-      <p style="font-size: 14px;color: #c9c9c9;">简单说就是云度蓝色越多越好！视宁度越小越好！</p>
+      <p style="font-size: 14px;color: #333333;">使用说明</p>
+      <p style="font-size: 14px;color: #333333;">简单说就是云度蓝色越多越好！视宁度越小越好！</p>
       <div style="width: 100%;overflow: scroll">
         <img src="./timerDESC.jpg" alt="">
       </div>
