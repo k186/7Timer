6865b58ab6b950556762ff18c7c02ed0bbcdfb28
k186 <k1868548@gmail.com>
2017-07-03 17:11:58 +0800

    map update

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,4 @@
 <template>
-  <router-view style="position: fixed;width: 100%"></router-view>
+  <router-view style="position: absolute;width: 100%"></router-view>
 </template>
 <style lang="scss" src="./main.scss"></style>
